263a8d7911f92400dda2581259d7670a1d392533
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-06-26 21:12:29 -0400

    HTML written for these components and nested in the App.js file.

---
+++ src/components/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div>
+      <a href="https://github.com/HankRichter">GitHub</a>
+      <a href="https://www.linkedin.com/in/edward-richter-291785152/">
+        Linkedin
+      </a>
+      <a href="https://twitter.com/">Twitter</a>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Navigation.js
@@ -0,0 +1,18 @@
+import React from "react";
+import Project from "./Project";
+
+function Navigation() {
+  return (
+    <nav>
+      <h1>Hank Richter</h1>
+      <ul>
+        <li>About Me</li>
+        <li>Portfolio</li>
+        <li>Contact</li>
+        <li>Resume</li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navigation;
