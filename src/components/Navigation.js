3e78d040ad9853c01be354a6757da0f289ddab8e
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-06-28 23:07:38 -0400

    Added the a tags for navigation through components.

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,15 +1,49 @@
 import React from "react";
 import Project from "./Project";
 
-function Navigation() {
+function Navigation({ currentPage, handlePageChange }) {
   return (
     <nav>
       <h1>Hank Richter</h1>
       <ul>
-        <li>About Me</li>
-        <li>Portfolio</li>
-        <li>Contact</li>
-        <li>Resume</li>
+        <li>
+          About Me
+          <a
+            href="#about"
+            onClick={() => handlePageChange("About")}
+            className={currentPage === "About" ? "nav-link active" : "nav-link"}
+          ></a>
+        </li>
+        <li>
+          Portfolio
+          <a
+            href="#portfolio"
+            onClick={() => handlePageChange("Portfolio")}
+            className={
+              currentPage === "Portfolio" ? "nav-link active" : "nav-link"
+            }
+          ></a>
+        </li>
+        <li>
+          Contact
+          <a
+            href="#contact"
+            onClick={() => handlePageChange("Contact")}
+            className={
+              currentPage === "Contact" ? "nav-link active" : "nav-link"
+            }
+          ></a>
+        </li>
+        <li>
+          Resume
+          <a
+            href="#resume"
+            onClick={() => handlePageChange("Resume")}
+            className={
+              currentPage === "Resume" ? "nav-link active" : "nav-link"
+            }
+          ></a>
+        </li>
       </ul>
     </nav>
   );
