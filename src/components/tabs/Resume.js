7eafce18fc687c401c10ab6a6f30a66a84098a50
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-06-30 22:28:40 -0400

    HTML updates for the Resume page

--- src/components/tabs/Resume.js
+++ src/components/tabs/Resume.js
@@ -1,7 +1,39 @@
 import React from "react";
 
 function Resume() {
-  return <p>Test 4</p>;
+  return (
+    <>
+      <h2>Resume</h2>
+      <div>
+        {/* Add Resume link */}
+        <p>Download my Resume</p>
+      </div>
+      <div>
+        <h3>Front-End Knowledge</h3>
+        <ul>
+          <li>List Item 1</li>
+          <li>List Item 1</li>
+          <li>List Item 1</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Back-End Knowledge</h3>
+        <ul>
+          <li>List Item 1</li>
+          <li>List Item 1</li>
+          <li>List Item 1</li>
+        </ul>
+      </div>
+      <div>
+        <h3>Proficiencies</h3>
+        <ul>
+          <li>List Item 1</li>
+          <li>List Item 1</li>
+          <li>List Item 1</li>
+        </ul>
+      </div>
+    </>
+  );
 }
 
 export default Resume;
