3a3d84bd3aad93176314181c243d15ccec0e8548
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-06-30 22:28:24 -0400

    HTML updates for the About page

--- src/components/tabs/About.js
+++ src/components/tabs/About.js
@@ -1,7 +1,21 @@
 import React from "react";
 
 function About() {
-  return <p>Test 1</p>;
+  return (
+    <>
+      <h2>About Me</h2>
+      <div>
+        <img src="route to picture" alt="Portrait of Hank Richter" />
+        <p>
+          My name is Hank Richter. I am currently a Network Technician at a
+          Michigan-based ISP called 123Net. I love opportunities where I can be
+          creative, as well as deplore my problem-solving skills. Thus, I have
+          turned to a career in software development. I hope to continually
+          update this page with projects to demonstrate my growth.
+        </p>
+      </div>
+    </>
+  );
 }
 
 export default About;
