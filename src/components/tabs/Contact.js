4236782546e3ded771b7746fe6a242a699dac14e
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-06-30 22:28:31 -0400

    HTML updates for the Contact page

--- src/components/tabs/Contact.js
+++ src/components/tabs/Contact.js
@@ -1,7 +1,28 @@
 import React from "react";
 
 function Contact() {
-  return <p>Test 3</p>;
+  return (
+    <>
+      <h2>Contact</h2>
+      <form>
+        <p>Name:</p>
+        <div>
+          <input type="text" placeholder="Name" name="name" required />
+        </div>
+        <p>Email:</p>
+        <div>
+          <input type="email" placeholder="Email" name="email" required />
+        </div>
+        <p>Message:</p>
+        <div>
+          <textarea placeholder="Your message" name="message" required />
+        </div>
+        <div>
+          <button type="submit">Send a message</button>
+        </div>
+      </form>
+    </>
+  );
 }
 
 export default Contact;
