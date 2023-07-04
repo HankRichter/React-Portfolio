3313ab4e1af60d00e9d5db414967c52c7cd4c6a3
Hank Richter <hankrichter97@gmail.com>
2023-07-04 12:02:49 -0400

    Wrote an array of objects for the projects.

--- src/components/tabs/Portfolio.js
+++ src/components/tabs/Portfolio.js
@@ -1,5 +1,22 @@
 import React from "react";
 
+const projectData = [
+  {
+    id: 1,
+    // img: img,
+    title: "Weather app",
+    production:"https://hankrichter.github.io/Weather-app/",
+    github: "https://github.com/HankRichter/Weather-app"
+  },
+  {
+    id: 2,
+    // img: img,
+    title: "Spin Cycle",
+    production: "https://spin-cycle.herokuapp.com/",
+    github: "https://github.com/Tagne13/Spin-Cycle"
+  }
+]
+
 function Portfolio() {
   return (
     <>
