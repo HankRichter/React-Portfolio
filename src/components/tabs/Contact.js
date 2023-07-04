93c5b3ddf5c2dbe3bab8425674f5c68024fb4c55
Hank Richter <hankrichter97@gmail.com>
2023-07-04 12:02:17 -0400

    Wrote the functions to handle a valid email is entered and to make sure a form input is not left blank.

--- src/components/tabs/Contact.js
+++ src/components/tabs/Contact.js
@@ -1,21 +1,63 @@
-import React from "react";
+import React, { useState} from "react";
+import {validateEmail, checkBlank} from "../../utils/helper"
 
 function Contact() {
+  const [formState, setFormState] = useState({
+    name: "",
+    email: "",
+    message: ""
+  });
+
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const {name, email, message} = formState;
+
+  function handleChange(e){
+    if (e.target.name === "email") {
+      const isValid = validateEmail(e.target.value);
+      if (!isValid) {
+        setErrorMessage("A valid email address is needed")
+      } else {
+        if (!e.target.value.length) {
+          setErrorMessage(`${e.target.name} is required`)
+        }else {
+          setErrorMessage("")
+        }
+      }
+    }
+    if (!errorMessage) {
+      setFormState({...formState, [e.target.name]: e.target.value});
+    }
+  };
+
+  function handleBlank(e){
+    if (e.target.name === "name" || e.target.name === "message") {
+      if (!e.target.value.length) {
+        setErrorMessage(`${e.target.name} is required`);
+      } else {
+        setErrorMessage("")
+      }
+    };
+    if (!errorMessage) {
+      setFormState({...formState, [e.target.name]: e.target.value})
+    }
+  };
+
   return (
     <>
       <h2>Contact</h2>
       <form>
         <p>Name:</p>
         <div>
-          <input type="text" placeholder="Name" name="name" required />
+          <input type="text" placeholder="Name" name="name" onBlur={handleBlank} required />
         </div>
         <p>Email:</p>
         <div>
-          <input type="email" placeholder="Email" name="email" required />
+          <input type="email" placeholder="Email" name="email" onBlur={handleChange} required />
         </div>
         <p>Message:</p>
         <div>
-          <textarea placeholder="Your message" name="message" required />
+          <textarea placeholder="Your message" name="message" onBlur={handleBlank} required />
         </div>
         <div>
           <button type="submit">Send a message</button>
