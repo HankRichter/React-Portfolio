607eff3efce02aec21bdce5250bf47fad3a91da8
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-07-10 22:49:18 -0400

    Added color schema to the Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@ module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+    colors: {
+      color1: "#191825",
+      color2: "#453C67",
+      color3: "#867CAF",
+      color4: "#F3F2F7",
+    },
   },
   plugins: [],
 };
