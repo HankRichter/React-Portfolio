7c65a5a846a8c5786510c0ca66fa89cb0e7f8983
Hank Richter <hankrichter97@gmail.com>
2023-07-11 15:57:41 -0400

    Styled the Footer.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,13 +2,14 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer>
-      <div className="bg-blue flex space-x-4">
-        <a href="https://github.com/HankRichter">GitHub</a>
-        <a href="https://www.linkedin.com/in/edward-richter-291785152/">
+    <footer className="flex flex-col items-center py-2 bg-color2 text-base text-color4 max-h-full md:text-lg">
+      <p className="mb-3">Made by Hank Richter</p>
+      <div className="flex">
+        <a className="mx-5 px-2 border-2 rounded hover:bg-color1" href="https://github.com/HankRichter">GitHub</a>
+        <a className="mx-5 px-2 border-2 rounded hover:bg-color1" href="https://www.linkedin.com/in/edward-richter-291785152/">
           Linkedin
         </a>
-        <a href="https://open.spotify.com/user/erichte1?si=12b3009059b5450f">
+        <a className="mx-5 px-2 border-2 rounded hover:bg-color1" href="https://open.spotify.com/user/erichte1?si=12b3009059b5450f">
           Spotify
         </a>
       </div>
