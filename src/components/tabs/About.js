c43a74d280500149973eef51f8510cfb0b16f1f4
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-07-12 22:50:51 -0400

    Updated the About style
    Home PC prettier changes

--- src/components/tabs/About.js
+++ src/components/tabs/About.js
@@ -1,13 +1,14 @@
 import React from "react";
+import Portrait from "../assets/Portrait.jpg";
 
 function About() {
   return (
     <>
-      <section className="flex-1 bg-color3 max-h-full">
-        <h2>About Me</h2>
+      <section className="flex-1 p-3 bg-color3">
+        <h2 className="text-3xl font-semibold mb-2">About Me</h2>
         <div>
-          <img src="route to picture" alt="Portrait of Hank Richter" />
-          <p>
+          <img src={Portrait} alt="Portrait of Hank Richter" />
+          <p className="text-lg mt-3">
             My name is Hank Richter. I am currently a Network Technician at a
             Michigan-based ISP called 123Net. I love opportunities where I can
             be creative, as well as deplore my problem-solving skills. Thus, I
