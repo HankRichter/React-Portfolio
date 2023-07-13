735f492946fa0dbf40cff3349270a3c98bac5305
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-07-12 22:51:14 -0400

    Updated the Resume style
    Home PC prettier changes

--- src/components/tabs/Resume.js
+++ src/components/tabs/Resume.js
@@ -4,15 +4,19 @@ import ResumePDF from "../assets/resume.pdf";
 function Resume() {
   return (
     <>
-      <section className="bg-color3">
-        <h2>Resume</h2>
-        <div>
-          <a href={ResumePDF} download>
+      <section className="flex-1 p-3 bg-color3">
+        <h2 className="text-3xl font-semibold mb-2">Resume</h2>
+        <div className="flex-1 py-3 bg-color3">
+          <a
+            href={ResumePDF}
+            download
+            className="p-1 border-2 rounded hover:bg-color2 hover:text-color4"
+          >
             Download my Resume
           </a>
         </div>
-        <div>
-          <h3>Front-End Knowledge</h3>
+        <div className="mb-2">
+          <h3 className=" text-xl font-semibold">Front-End Knowledge</h3>
           <ul>
             <li>HTML</li>
             <li>CSS</li>
@@ -21,8 +25,8 @@ function Resume() {
             <li>React</li>
           </ul>
         </div>
-        <div>
-          <h3>Back-End Knowledge</h3>
+        <div className="mb-2">
+          <h3 className=" text-xl font-semibold">Back-End Knowledge</h3>
           <ul>
             <li>Node.js</li>
             <li>Express.js</li>
@@ -30,8 +34,8 @@ function Resume() {
             <li>MongoDB</li>
           </ul>
         </div>
-        <div>
-          <h3>Proficiencies</h3>
+        <div className="mb-2">
+          <h3 className=" text-xl font-semibold">Proficiencies</h3>
           <ul>
             <li>PWAs (Progressive Web Apps)</li>
             <li>Git (Version Control)</li>
