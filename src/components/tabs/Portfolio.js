5a4fca7090011631ed92e8aec42290bddc5d7c9a
Hank Richter <hankrichter97@gmail.com>
2023-07-13 18:36:34 -0400

    Further updates to Portfolio styling.

--- src/components/tabs/Portfolio.js
+++ src/components/tabs/Portfolio.js
@@ -23,17 +23,17 @@ function Portfolio() {
   return (
     <section className="flex-1 p-3 bg-color3">
       <h2 className="text-3xl font-semibold mb-2">Portfolio</h2>
-      <div className="flex w-96">
+      <div className="md:flex">
         {projectData.map(({ id, img, title, production, github }) => {
           return (
-            <article className="border-2" key={id}>
+            <article className="my-2 pb-3 border-2 md:m-2" key={id}>
               <img src={img} alt={title} />
-              <h3>{title}</h3>
-              <div>
-                <a href={production} target="_blank">
+              <h3 className=" text-xl font-semibold m-2">{title}</h3>
+              <div className="mt-3">
+                <a href={production} target="_blank" className="m-2 p-1 border-2 rounded hover:bg-color2 hover:text-color4">
                   Production
                 </a>
-                <a href={github} target="_blank">
+                <a href={github} target="_blank" className="m-2 p-1 border-2 rounded hover:bg-color2 hover:text-color4">
                   Github
                 </a>
               </div>
