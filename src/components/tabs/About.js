599882b4291f69c09b42c3260c6df8ee04715214
Hank Richter <120127039+HankRichter@users.noreply.github.com>
2023-07-13 20:49:13 -0400

    Finishing touches for styling.
    Home PC prettier changes.

--- src/components/tabs/About.js
+++ src/components/tabs/About.js
@@ -4,11 +4,11 @@ import Portrait from "../assets/Portrait.jpg";
 function About() {
   return (
     <>
-      <section className="flex-1 p-3 bg-color3">
-        <h2 className="text-3xl font-semibold mb-2">About Me</h2>
+      <section className="flex-1 p-3 bg-color3 md:flex md:justify-center">
         <div>
+          <h2 className="text-3xl font-semibold mb-2">About Me</h2>
           <img src={Portrait} alt="Portrait of Hank Richter" />
-          <p className="text-lg mt-3">
+          <p className="text-lg mt-3 md:w-96">
             My name is Hank Richter. I am currently a Network Technician at a
             Michigan-based ISP called 123Net. I love opportunities where I can
             be creative, as well as deplore my problem-solving skills. Thus, I
--- src/components/tabs/Resume.js
+++ src/components/tabs/Resume.js
@@ -4,44 +4,46 @@ import ResumePDF from "../assets/resume.pdf";
 function Resume() {
   return (
     <>
-      <section className="flex-1 p-3 bg-color3">
-        <h2 className="text-3xl font-semibold mb-2">Resume</h2>
-        <div className="flex-1 py-3 bg-color3">
-          <a
-            href={ResumePDF}
-            download
-            className="p-1 border-2 rounded hover:bg-color2 hover:text-color4"
-          >
-            Download my Resume
-          </a>
-        </div>
-        <div className="mb-2">
-          <h3 className=" text-xl font-semibold">Front-End Knowledge</h3>
-          <ul>
-            <li>HTML</li>
-            <li>CSS</li>
-            <li>JavaScript</li>
-            <li>Tailwind</li>
-            <li>React</li>
-          </ul>
-        </div>
-        <div className="mb-2">
-          <h3 className=" text-xl font-semibold">Back-End Knowledge</h3>
-          <ul>
-            <li>Node.js</li>
-            <li>Express.js</li>
-            <li>MySQL</li>
-            <li>MongoDB</li>
-          </ul>
-        </div>
-        <div className="mb-2">
-          <h3 className=" text-xl font-semibold">Proficiencies</h3>
-          <ul>
-            <li>PWAs (Progressive Web Apps)</li>
-            <li>Git (Version Control)</li>
-            <li>OOP (Object-Oriented Programming)</li>
-            <li>MVC (Model-View-Controller Paradigm)</li>
-          </ul>
+      <section className="flex-1 p-3 bg-color3 md:flex md:justify-center">
+        <div>
+          <h2 className="text-3xl font-semibold mb-2">Resume</h2>
+          <div className="flex-1 py-3 bg-color3">
+            <a
+              href={ResumePDF}
+              download
+              className="p-1 border-2 rounded hover:bg-color2 hover:text-color4"
+            >
+              Download my Resume
+            </a>
+          </div>
+          <div className="mb-2">
+            <h3 className=" text-xl font-semibold">Front-End Knowledge</h3>
+            <ul>
+              <li>HTML</li>
+              <li>CSS</li>
+              <li>JavaScript</li>
+              <li>Tailwind</li>
+              <li>React</li>
+            </ul>
+          </div>
+          <div className="mb-2">
+            <h3 className=" text-xl font-semibold">Back-End Knowledge</h3>
+            <ul>
+              <li>Node.js</li>
+              <li>Express.js</li>
+              <li>MySQL</li>
+              <li>MongoDB</li>
+            </ul>
+          </div>
+          <div className="mb-2">
+            <h3 className=" text-xl font-semibold">Proficiencies</h3>
+            <ul>
+              <li>PWAs (Progressive Web Apps)</li>
+              <li>Git (Version Control)</li>
+              <li>OOP (Object-Oriented Programming)</li>
+              <li>MVC (Model-View-Controller Paradigm)</li>
+            </ul>
+          </div>
         </div>
       </section>
     </>
